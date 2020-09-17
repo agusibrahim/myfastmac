6607ec8475bdb464d53c15b033437bc22d617732
Agus Ibrahim <pymobile123@gmail.com>
2020-09-17 11:02:44 +0700

    add ngrok to mac

--- script-mac.sh
+++ script-mac.sh
@@ -1,6 +1,3 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
-wget "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" -O temp.zip
-unzip temp.zip
-rm temp.zip
-./ngrok authtoken 2wQnCPcK7LCH9T2pdkK3C_6uPyNuK72myGuVQ2srPNS
+brew cask install ngrok
