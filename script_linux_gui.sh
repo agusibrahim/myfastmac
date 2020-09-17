45f2bdbc4961dd5d702839b2644e1064938e0f9b
Agus Ibrahim <pymobile123@gmail.com>
2020-09-17 15:46:02 +0700

    Update script_linux_gui.sh

--- script_linux_gui.sh
+++ script_linux_gui.sh
@@ -2,9 +2,11 @@ sudo apt-get install nano xorg lxde-core tightvncserver
 mkdir -p ~/.vnc/
 cp -rf passwd ~/.vnc/1passwd
 cp -rf xstartup ~/.vnc/1xstartup
-#vncserver -depth 24 -geometry 1280x768
+chmod 600 ~/.vnc/passwd
+chmod 755 ~/.vnc/xstartup
+vncserver -depth 24 -geometry 1280x768
 wget "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" -O temp.zip
 unzip temp.zip
 rm temp.zip
-./ngrok authtoken 2wQnCPcK7LCH9T2pdkK3C_6uPyNuK72myGuVQ2srPNS
-./ngrok tcp 5900 > /dev/null &
+#./ngrok authtoken 2wQnCPcK7LCH9T2pdkK3C_6uPyNuK72myGuVQ2srPNS
+#./ngrok tcp 5900 > /dev/null &
