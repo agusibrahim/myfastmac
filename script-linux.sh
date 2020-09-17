67c00569f18f91defa0ec080dcf2444877a0dd40
Agus Ibrahim <pymobile123@gmail.com>
2020-09-17 11:01:20 +0700

    add ngrok

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,4 @@
-echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+wget "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" -O temp.zip
+unzip temp.zip
+rm temp.zip
+./ngrok authtoken 2wQnCPcK7LCH9T2pdkK3C_6uPyNuK72myGuVQ2srPNS
