fab03751cd856ff6e28b6397a7e4db876a5b42af
Agus Ibrahim <pymobile123@gmail.com>
2020-09-17 16:15:07 +0700

    Update script_linux_gui.sh

--- script_linux_gui.sh
+++ script_linux_gui.sh
@@ -4,7 +4,14 @@ cp -rf passwd ~/.vnc/passwd
 cp -rf xstartup ~/.vnc/xstartup
 chmod 600 ~/.vnc/passwd
 chmod 755 ~/.vnc/xstartup
+rm passwd
+rm xstartup
 vncserver -depth 24 -geometry 1280x768
 wget "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" -O temp.zip
 unzip temp.zip
 rm temp.zip
+./ngrok authtoken 1T750atJi3xccndeUqJ4ewiS62o_2s6f8GUccL1qDUXTGSftN
+./ngrok tcp 5900 > /dev/null &
+sleep 3
+curl http://127.0.0.1:4040/api/tunnels>ngrok.json
+cat ngrok.json
