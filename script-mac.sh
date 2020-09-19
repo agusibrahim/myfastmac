9bd4163e340720d37e4f1b42972dfe936b3f15f2
Agus Ibrahim <pymobile123@gmail.com>
2020-09-19 21:59:25 +0700

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,7 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 brew cask install ngrok
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvncpw -vncpw agus111
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
