e5af41b1510ebe0a3179419c8eca06904bf5c299
Agus Ibrahim <pymobile123@gmail.com>
2020-09-19 22:43:45 +0700

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,4 +1,1 @@
-wget "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" -O temp.zip
-unzip temp.zip
-rm temp.zip
-./ngrok authtoken 2wQnCPcK7LCH9T2pdkK3C_6uPyNuK72myGuVQ2srPNS
+sudo snap install ngrok
