dfbcaaa87e0e424d9b9746161e185671e2241511
Agus Ibrahim <pymobile123@gmail.com>
2020-09-20 21:39:02 +0700

    Update script_linux_gui.sh

--- script_linux_gui.sh
+++ script_linux_gui.sh
@@ -1,17 +1,10 @@
-sudo apt-get install nano xorg lxde-core tightvncserver
-mkdir -p ~/.vnc/
-cp -rf passwd ~/.vnc/passwd
-cp -rf xstartup ~/.vnc/xstartup
-chmod 600 ~/.vnc/passwd
-chmod 755 ~/.vnc/xstartup
-rm passwd
-rm xstartup
-vncserver -depth 24 -geometry 1280x768
-wget "https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip" -O temp.zip
-unzip temp.zip
-rm temp.zip
-./ngrok authtoken 1T750atJi3xccndeUqJ4ewiS62o_2s6f8GUccL1qDUXTGSftN
-./ngrok tcp 5900 > /dev/null &
-sleep 3
-curl http://127.0.0.1:4040/api/tunnels>ngrok.json
-cat ngrok.json
+sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils xrdp
+sudo adduser xrdp ssl-cert
+sudo systemctl restart xrdp
+echo 'root:agus111' | sudo chpasswd
+ngrok authtoken 1hdFJmQC6iIak1eSbqx1t7Rrx56_2JLpasDVHybBixWv7Xftm
+ngrok tcp 3389
+sleep 2
+echo "RDP HOST:" $(curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url|cut -c7-)
+echo “USERNAME: root”
+echo “PASSWORD: agus111”
