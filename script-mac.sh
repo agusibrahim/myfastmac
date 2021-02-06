e92006c1c8f818e82f787fcf5ef352b8d78c7ab8
Agus Ibrahim <pymobile123@gmail.com>
2021-02-06 21:27:19 +0700

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -4,7 +4,7 @@ brew install ngrok --cask
 brew install code-server
 code-server --auth none>/dev/null &
 ngrok authtoken 3FqC8NZYFm3FCTnq31YP_4PVTJD76Ugzejfn5Eq49A
-ngrok http 8080
+ngrok http 8080>/dev/null &
 sleep 10
 curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
