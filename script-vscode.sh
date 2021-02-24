0dbf18aae80a0b45cc95cc031045c57a69d12ebe
Agus Ibrahim <pymobile123@gmail.com>
2021-02-24 13:57:06 +0700

    Update script-vscode.sh

--- script-vscode.sh
+++ script-vscode.sh
@@ -5,3 +5,6 @@ ngrok authtoken 3FqC8NZYFm3FCTnq31YP_4PVTJD76Ugzejfn5Eq49A
 ngrok http 8080 >/dev/null &
 sleep 10
 curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+while ! test -f "/tmp/stopnow"; do
+  sleep 10
+done
